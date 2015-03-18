f29fb42fd697742e8b4798d5fcd8bd48c44d274b
James Treasure <jandrewtreasure@gmail.com>
2015-03-18 08:37:41 +0000

    Fixed multiple answers

--- Coursework.pl
+++ Coursework.pl
@@ -9,96 +9,7 @@ The following conversation takes place:
 (d) S: Now I know the two numbers.
 */
 
-
 %-------s1--------
-/*The goal s1(Q,100) will bind Q with a list of quadruples [X,Y,S,P],
- where S=X+Y and P=X*Y and X and Y are possible solutions after 
- sentence (a) is pronounced. The list will be ordered by ascending values of S.
-
-Q = [[3, 4, 7, 12], [2, 6, 8, 12], [4, 5, 9, 20], [3, 6, 9, 18], 
-[4, 6, 10, 24], [5, 6, 11, 30], [4, 7, 11, 28], [3, 8, 11, 24], 
-[2, 9, 11, 18], [4, 8, 12, 32], [2, 10, 12, 20], [6, 7, 13, 42], 
-[5, 8, 13, 40], [4, 9, 13, 36], [3, 10, 13, 30], [6, 8, 14, 48], 
-
-step 1: using Sieve of Eratosthenes method to get prime number list which is less
-than 100
-step 2: geting composite number list according to prime number list	which is less
-than 100
-step 3: combining the prime number list with prime list[P,C,P+C,P*C] and 
-combining the composite number list with composite number list[C1,C2,C1+C2,C1*C2].
-In this step,firstly I am going to get rid of the pairs which its sum
-more than 100. Secondly, after adding the first filter, I am using mergesort 
-to order the list by it's product,afterthen,I will move out the list which it's 
-product is single(compare the product of the list and it's next list, if they are
-equal, then save all the list which have the same product with the first list.
-Otherwise, delete that list).
-
-*/
-
-%Generates's a list of numbers
-%generateNumbers(10,L).
-generate_numbers(0,[]).
-generate_numbers(N,[Head|Tail]):-
-					N > 0,						%N must be greater than 0. Base case.
-                    Head is N,					%Sets the head of list to N
-                    N1 is N-1,					%Decrements N by 1
-                    generate_numbers(N1,Tail).	%Recurses on generateNumbers
-
-
-prime(2).
-prime(N) :- 
-	sqrt(N,S), 		%Square root N and store in S.
-	prime_checker(N,S,3).	%Pass N, Square root of N and 3 to prime_checker.
-
-prime_checker(_,S,D) :- D>S.	%D greater than square root of N.
-prime_checker(N,S,D) :-
-	N =\= D*(N//D), %N not equal to D*(N divided by D (integer division))
-	D1 is D+2,		%Add two to D. Means even numbers are never prime.
-	prime_checker(N,S,D1).	%Recurse with new D1
-
-%Generates prime numbers between two numbers. prime_list(1,100,A).
-prime_list(A,B,L) :-
-	 A =< 2,		%If A is less than two
-	 !, 			
-	 p_list(2,B,L).	%Calls p_list with A replaces by 2.
-
-prime_list(A,B,L) :-
-	 A1 is (A // 2) * 2 + 1,	%Essentially makes A1 always odd as even not prime.
-	 p_list(A1,B,L).			%Calls p_list with A1
-
-p_list(A,B,[]) :-	%Empty list if A is bigger than B. eg, (100,10,A).
-	 A > B, 		%Base case for when to stop recursing.
-	 !.
-
-%If it returns true then add A to the prime list.
-p_list(A,B,[A|L]) :- 
-	prime(A), 		%Checks if first number is prime.
-	!, 
-   	next(A,A1),		%Adds two to A and returns A1.
-   	p_list(A1,B,L). %Recursive call with new value A1.
-
-%If A wasn't prime in the prime check above, add two to A and recurse.
-p_list(A,B,L) :- 
-   next(A,A1),
-   p_list(A1,B,L).
-
-next(2,3) :- !.
-next(A,A1) :- A1 is A + 2.
-
-
-my_subtract([], _, []).
-my_subtract([Head|Tail], L2, L3) :-
-    my_membership(Head, L2),
-    !,
-    my_subtract(Tail, L2, L3).
-my_subtract([Head|Tail1], L2, [Head|Tail3]) :-
-    my_subtract(Tail1, L2, Tail3).
-
-my_composites(Start,End,Comp):-
-	generate_numbers(End,Full),
-	prime_list(Start,End,Primes),
-	my_subtract(Full,Primes,Comp).
-
 my_between(N, M, K) :- 
 	N =< M, 
 	K = N.
@@ -107,42 +18,9 @@ my_between(N, M, K) :-
 	N1 is N+1, 
 	my_between(N1, M, K).
 
-values_p_can_be(Start,End,Final):-
-	generate_numbers(End,Full),
-	check_div(Start,End,List),
-	my_subtract(Full,List,Final).
-
-
-
-check_div(Start,End,[]):-
-	Start > End,!.
-check_div(Start,End,[Start|L]):-
-	divisors(Start,List),
-	length(List,GetLength),
-	GetLength < 5,
-	mynext(Start,Start1),
-	check_div(Start1,End,L).
-check_div(Start,End,L):-
-	mynext(Start,Start1),
-	check_div(Start1,End,L).
-
-getLength(Number,Length):-
-	divisors(Number,Div),
-	length(Div,Length).
-
-divisors(X, Divs) :- 
-	bagof(D,divs(X,D), Divs).
-divs(X,D) :- 
-	my_between(1,X,D), 
-	0 is X mod D.
-
-mynext(2,3) :- !.
-mynext(A,A1) :- A1 is A + 1.
-
 gen_numbers(MaxSum, Result) :-
     MaxSum> 1,								
     gen_numbers(2, MaxSum, 2, Result).		
-
 gen_numbers(Sum, Sum, Sum, []).            % 'Sum' has reached max value
 gen_numbers(Sum, MaxSum, Sum, Result) :-   
     Sum < MaxSum,
@@ -152,57 +30,50 @@ gen_numbers(Sum, MaxSum, X, [[X, Y, Sum, P]|Result]) :-
 	Sum =< MaxSum,	
 	X < Sum,
 	Y is Sum - X, 
-	X < Y, 
+	X < Y,
 	P is X*Y,
 	X1 is X + 1, 
 	gen_numbers(Sum, MaxSum, X1, Result).
-
 gen_numbers(Sum, MaxSum, X, Result) :-
 	X1 is X + 1,
     gen_numbers(Sum, MaxSum, X1, Result).
 
-
-%member([_,_,Third,_] , [[a,b,c,d],[e,f,g,h],[i,j,k,l]]).
-remove_unique(Start,End,Final):-
+test(End,Final):-
 	gen_numbers(End,Numbers),
-	my_membership(Numbers,Final).
+	length(Numbers,Final).
 	
-
-test(Start,End,Final):-
-	gen_numbers(End,Numbers),
-	my_membership(Numbers,Remove),
-	length(Remove,Final).
-
-delMember(_, [], []).
-delMember(X, [X|Xs], Y) :-
-	delMember(X, Xs, Y), !.
-delMember(X, [T|Xs], [T|Y]) :-
-	delMember(X, Xs, Y).
-
-
-element_at(X, List, Pos) :-
-    element_at(X, List, 1, Pos).
-element_at(X, [X|_], Pos, Pos).
-element_at(X, [_|T], Acc, Pos) :-
-    Acc1 is Acc + 1,
-    element_at(X, T, Acc1, Pos).
-
 last([X], X).
 last([_|T], X) :- last(T, X).
  
 forth([_,_,_,F|_], F).
  
 matching_forth([], _, 0).
-matching_forth([H|T], X, R) :- forth(H, X), matching_forth(T, X, RR), R is RR + 1.
-matching_forth([_|T], X, R) :- matching_forth(T, X, R).
+matching_forth([H|T], X, R) :- 
+	forth(H, X), 
+	matching_forth(T, X, RR), 
+	R is RR + 1.
+matching_forth([_|T], X, R) :- 
+	matching_forth(T, X, R).
  
 my_membership([], [], _).
  
-my_membership([H|T], [H|R], A) :- last(H, X), matching_forth(A, X, C), C > 1, my_membership(T, R, A).
+my_membership([H|T], [H|R], A) :- 
+	last(H, X), 
+	matching_forth(A, X, C),
+	C > 1, 
+	my_membership(T, R, A).
  
-my_membership([_|T], R, A) :- my_membership(T, R, A).
+my_membership([_|T], R, A) :- 
+	my_membership(T, R, A).
  
-my_membership(L, R) :-  my_membership(L, R, L).
+my_membership(L, R) :-  
+	my_membership(L, R, L), !.
+ 
+s1(Q,N):-
+	gen_numbers(N,Numbers), !,
+	my_membership(Numbers,Q).
+
+%-------s2--------
 
 
 
